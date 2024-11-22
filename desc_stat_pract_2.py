4fd23bc95c6d99d97141ef6e0629f0c710b719d2
jesusvrdz <jesus.vallejoro@uanl.edu.mx>
2024-11-22 03:57:04 -0600

    Practica_2_Estadística_Descriptiva
    Análisis descriptivo de datos, donde se agrupó por género, país, director y fecha de agregación.

---
+++ desc_stat_pract_2.py
@@ -0,0 +1,61 @@
+#Estadística descriptiva
+import pandas as pd
+
+df = pd.read_csv("movies_clean.csv")
+
+#Análisis para observar la relación de la duración con el género
+def EstadisticasPorGeneroYDuracion():
+    #Número de géneros únicos
+    #total_generos = df['listed_in'].nunique()
+    #print(f"Total de géneros únicos: {total_generos}\n")
+
+    #Agrupamos las estadísticas por género
+    resultado = df.groupby('listed_in')['duration'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Peliculas', 'Duracion_Promedio', 'Duracion_Minima', 'Duracion_Maxima', 'Desviacion_Estandar', 'Varianza']
+    
+    #print(resultado)
+    resultado.to_csv('Tablas_pract_2/Dataset_Genero_Duracion.csv')
+
+
+#Análisis para observar la relación de la duración promedio por país
+def EstadisticasPorPaisYDuracion():
+    #Duración promedio por país
+    resultado = df.groupby('country')['duration'].agg(['mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Duracion_Promedio', 'Duracion_Minima', 'Duracion_Maxima', 'Desviacion_Estandar', 'Varianza']
+    
+    #print(resultado) 
+    resultado.to_csv('Tablas_pract_2/Dataset_Pais_Duracion.csv')
+
+
+#Análisis para observar la estadisticas de los directores
+def EstadisticasPorDirector():
+    #Agrupamos por director y calculamos las estadísticas para la cantidad de películas
+    resultado = df.groupby('director')['duration'].agg(['count', 'mean', 'min', 'max', 'std', 'var'])
+    resultado.columns = ['Total_Peliculas', 'Duracion_Promedio', 'Duracion_Minima', 'Duracion_Maxima', 'Desviacion_Estandar', 'Varianza']
+    
+    #print(resultado)
+    resultado.to_csv('Tablas_pract_2/Dataset_Director_Peliculas_Duracion.csv')
+
+
+#Análisis para observar cuantas peliculas se agregan por mes
+def EstadisticasPorFechaAgregada():
+    df['date_added'] = pd.to_datetime(df['date_added'], errors='coerce')
+    #Extraemos el mes en el que se añadieron las películas
+    df['month_added'] = df['date_added'].dt.month
+    
+    #Agrupamos por mes, contamos las películas y hacemos los cálculos
+    resultado = df.groupby('month_added').size().reset_index(name='Total_Peliculas')
+    resultado['Promedio_Peliculas'] = resultado['Total_Peliculas'].mean()
+    resultado['Peliculas_Minimas'] = resultado['Total_Peliculas'].min()
+    resultado['Peliculas_Maximas'] = resultado['Total_Peliculas'].max()
+    resultado['Desviacion_Estandar'] = resultado['Total_Peliculas'].std()
+    resultado['Varianza'] = resultado['Total_Peliculas'].var()
+
+    #print(resultado)
+    resultado.to_csv('Tablas_pract_2/Dataset_Peliculas_Agregadas_Por_Mes.csv')
+
+
+EstadisticasPorGeneroYDuracion()
+EstadisticasPorPaisYDuracion()
+EstadisticasPorDirector()
+EstadisticasPorFechaAgregada()
